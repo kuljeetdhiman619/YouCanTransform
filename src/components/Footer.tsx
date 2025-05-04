90e1fdba73d95bfcdcc15e05484df626390d0295
kuljeetdhiman619 <kuljeetdhiman619@gmail.com>
2025-05-04 17:59:53 +0530

    gitlink 4-5-25

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,7 @@ export const Footer = () => {
           <div>
             <a
               rel="noreferrer noopener"
-              href="https://github.com/leoMirandaa/shadcn-landing-page.git"
+              href="https://github.com/kuljeetdhiman619/YouCanTransform.git"
               className="opacity-60 hover:opacity-100"
             >
               Github
